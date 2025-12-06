c8ec0aa32737d4ac792a0e7ba5c30bd7a7bb5822
arohaz2005 <arohaz085@gmail.com>
2025-12-07 00:08:48 +0530

    User routes added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,10 @@
+export const protectRoute = async(req, res, next) => {
+
+    if(!req.auth().isAuthenticated) {
+        return res.status(401).json({
+            message: "Unauthorized! - you must log in"
+        })
+    }
+    next();
+}
+
