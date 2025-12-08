3049b0a1d2b6e5d88da3ade65a8bc0d477ab559d
arohaz2005 <arohaz085@gmail.com>
2025-12-08 16:38:21 +0530

    mobile auth added

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native";
- 
-export default function App() {
+import { View, Text, Button } from 'react-native'
+import { useClerk } from '@clerk/clerk-expo';
+
+const HomeScreen = () => {
+
+  const { signOut } = useClerk();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View>
+      <Text>Home Screen</Text>
+
+      <Button onPress={() => signOut()} title="logout"></Button>
     </View>
-  );
+  )
 }
+
+export default HomeScreen;
