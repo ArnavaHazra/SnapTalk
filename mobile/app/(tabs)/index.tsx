5bda822232bc62f012f2d70a30a4a4dbacf589c4
arohaz2005 <arohaz085@gmail.com>
2025-12-16 20:54:44 +0530

    Create posts added

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,14 +1,27 @@
+import PostComposer from "@/components/PostComposer"
 import SignOutButton from "@/components/SignOutButton"
 import { useUserSync } from "@/hooks/useUserSync"
-import { View, Text } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
+import { View, Text, ScrollView } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const HomeScreen = () => {
   useUserSync();
   return (
     <SafeAreaView className="flex-1">
-      <Text>HomeScreen</Text>
-      <SignOutButton />
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={26} color="#1DA1F2"/>
+        <Text className="text-xl font-bold text-gray-600">Home</Text>
+        <SignOutButton />
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={ {paddingBottom: 80} }
+      >
+        <PostComposer />
+      </ScrollView>
     </SafeAreaView>
   )
 }
